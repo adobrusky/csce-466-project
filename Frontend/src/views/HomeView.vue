9eb4fa9546744a4a8f86e568f6fcbe3e72406f5b
Jackz <jacksonbixby@gmail.com>
2023-05-01 14:31:04 -0500

    Update home page

--- Frontend/src/views/HomeView.vue
+++ Frontend/src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+  <div class="container">
+    <p class="title is-2">Welcome to Warehouse Management Panel</p>
+    <p class="subtitle is-4">Select a view above to begin</p>
   </div>
 </template>
 
